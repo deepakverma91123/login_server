d13dacd9779579ff2722bbc538fc5d76c5caf7aa
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-11-30 19:48:57 +0000

    Chore: Removed cors for tutorial purposes.

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ require('./config/db');
 const app = require('express')();
 const port = process.env.PORT || 3000;
 
-// Cors
-const cors = require('cors');
-app.use(cors());
-
 const UserRouter = require('./api/User');
 
 // For accepting post form data

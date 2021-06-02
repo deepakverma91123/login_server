466e5f5109c442304797574c690f7a0720fb79f1
cobytab <terra.andoh@gmail.com>
2021-06-02 10:34:35 +0000

    feat(Verification): :construction: Url to used in email modified
    Localhost port changed to current 5000

--- api/User.js
+++ api/User.js
@@ -140,7 +140,7 @@ router.post("/signup", (req, res) => {
 // send verification email
 const sendVerificationEmail = ({ _id, email }, res) => {
   // url to be used in the email.
-  const currentUrl = "http://localhost:3000/";
+  const currentUrl = "http://localhost:5000/";
 
   const uniqueString = uuidv4() + _id;
 
